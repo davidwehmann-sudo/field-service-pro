3b54fd1882c788b1fcc3dbdd406d469e10c6b450
David Wehmann <david.wehmann@wehmanntexas.com>
2026-01-22 19:47:10 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,5 +1,4 @@
 import Authorizations from './pages/Authorizations';
-import CustomerPortal from './pages/CustomerPortal';
 import Customers from './pages/Customers';
 import Dashboard from './pages/Dashboard';
 import FieldTech from './pages/FieldTech';
@@ -7,7 +6,6 @@ import Home from './pages/Home';
 import Invoices from './pages/Invoices';
 import PartsInventory from './pages/PartsInventory';
 import PartsOrders from './pages/PartsOrders';
-import PayInvoice from './pages/PayInvoice';
 import PaymentLog from './pages/PaymentLog';
 import SecurityChecklist from './pages/SecurityChecklist';
 import ServiceReports from './pages/ServiceReports';
@@ -20,7 +18,6 @@ import __Layout from './Layout.jsx';
 
 export const PAGES = {
     "Authorizations": Authorizations,
-    "CustomerPortal": CustomerPortal,
     "Customers": Customers,
     "Dashboard": Dashboard,
     "FieldTech": FieldTech,
@@ -28,7 +25,6 @@ export const PAGES = {
     "Invoices": Invoices,
     "PartsInventory": PartsInventory,
     "PartsOrders": PartsOrders,
-    "PayInvoice": PayInvoice,
     "PaymentLog": PaymentLog,
     "SecurityChecklist": SecurityChecklist,
     "ServiceReports": ServiceReports,
